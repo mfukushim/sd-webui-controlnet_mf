bf797361cc5634d3217e4d7530ff996728636deb
mfuku <daisycodes@gmail.com>
2023-08-02 22:41:11 +0900

    fix conflict

--- preload.py
+++ preload.py
@@ -1,18 +1,18 @@
 def preload(parser):
     parser.add_argument(
-        "--controlnet-dir",
+        "--controlnet-dir-mf",
         type=str,
         help="Path to directory with ControlNet models",
         default=None,
     )
     parser.add_argument(
-        "--controlnet-annotator-models-path",
+        "--controlnet-annotator-models-path-mf",
         type=str,
         help="Path to directory with annotator model directories",
         default=None,
     )
     parser.add_argument(
-        "--no-half-controlnet",
+        "--no-half-controlnet-mf",
         action="store_true",
         help="do not switch the ControlNet models to 16-bit floats (only needed without --no-half)",
         default=None,
@@ -20,13 +20,13 @@ def preload(parser):
     # Setting default max_size=16 as each cache entry contains image as both key
     # and value (Very costly).
     parser.add_argument(
-        "--controlnet-preprocessor-cache-size",
+        "--controlnet-preprocessor-cache-size-mf",
         type=int,
         help="Cache size for controlnet preprocessor results",
         default=16,
     )
     parser.add_argument(
-        "--controlnet-loglevel",
+        "--controlnet-loglevel-mf",
         default="INFO",
         choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
         help="Set the log level (DEBUG, INFO, WARNING, ERROR, CRITICAL)",
